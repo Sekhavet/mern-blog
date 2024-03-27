0eedcd874f10848bc2ec78b05fdee0a7d96eef67
Sekhavet <102406346+Sekhavet@users.noreply.github.com>
2024-03-27 04:33:41 +0400

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
-const app = express()
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO_URL
+  )
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
 
 app.listen(3000, () => {
-    console.log('Server: 3000')
-})
+  console.log("Server: 3000");
+});
