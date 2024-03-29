2b93bf257c559e273d2f449bc9671e7c02f66210
Sekhavet <102406346+Sekhavet@users.noreply.github.com>
2024-03-30 03:28:38 +0400

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import User from "../models/user.module.js"
+import bcryptjs from 'bcryptjs'
+
+export const signup = async (req, res) => {
+    const { username, email, password} = req.body;
+
+    if(!username || !email || !password || username === "" || email === "" || password === ""){
+        return res.status(400).json({ message: "All fields are required" })
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword
+    })
+    
+    try {
+        await newUser.save();
+        res.json("Signup succesful")
+    } catch (error) {
+       res.status(500).json({ message: error.message }) 
+    }
+}
