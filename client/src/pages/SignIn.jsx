c9edefd194d9d693c625b3fa041c6868817c2d85
Sekhavet <102406346+Sekhavet@users.noreply.github.com>
2024-04-28 15:17:33 +0400

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,67 +1,67 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignIn() {
-  const [formData, setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const navigate = useNavigate()
-
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData( { ...formData, [e.target.id]: e.target.value.trim() } )
-  }
-
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.')
+      return dispatch(signInFailure("Please fill all the fields"));
     }
     try {
-      setLoading(true)
-      setErrorMessage(null)
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message)
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false)
+
       if (res.ok) {
-        navigate('/')
+        dispatch(signInSuccess(data));
+        navigate("/");
       }
     } catch (error) {
-      setErrorMessage("This username already exists!");
-      setLoading(false)
+      dispatch(signInFailure(error.message));
     }
-  }
-   
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* Left */}
         <div className="flex-1">
-          <Link
-            to="/"
-            className="text-4xl font-bold dark:text-white"
-          >
+          <Link to="/" className="text-4xl font-bold dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               Sekhavet&apos;s
             </span>
             Blog
           </Link>
           <p className="text-sm mt-5">
-          This is a my MERN blog project. You can sign in with your email and password or with Google.
+            This is a my MERN blog project. You can sign in with your email and
+            password or with Google.
           </p>
         </div>
         {/* Right */}
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value="Your email"/>
+              <Label value="Your email" />
               <TextInput
                 type="email"
                 placeholder="name@company.com"
@@ -70,7 +70,7 @@ export default function SignIn() {
               />
             </div>
             <div>
-              <Label value="Your password"/>
+              <Label value="Your password" />
               <TextInput
                 type="password"
                 placeholder="********"
@@ -78,15 +78,19 @@ export default function SignIn() {
                 onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone='purpleToPink' type="submit" disabled={loading}>
-              {
-                loading ? (
-                  <>
-                    <Spinner size='sm'/>
-                    <span className="pl-3">Loading...</span>
-                  </>
-                ) : 'Sign in'
-              }
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign in"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -95,13 +99,11 @@ export default function SignIn() {
               Sign Up
             </Link>
           </div>
-          {
-            errorMessage && (
-              <Alert className="mt-5" color='failure'>
-                {errorMessage}
-              </Alert>
-            )
-          }
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
