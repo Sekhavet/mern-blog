dcc673e41486335d1c97d555cc9230e91a619387
Sekhavet <102406346+Sekhavet@users.noreply.github.com>
2024-05-07 00:27:42 +0400

    fix useEffect and unused dependency errors

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -27,37 +27,39 @@ export default function DashProfile() {
   };
   useEffect(() => {
     if (imageFile) {
+      const uploadImage = async () => {
+        setImageFileUploadError(null);
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + imageFile.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, imageFile);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+            setImageFileUploadProgress(progress.toFixed(0));
+          },
+          () => {
+            setImageFileUploadError(
+              "Could not upload image (File must be less than 2MB)"
+            );
+            setImageFileUploadProgress(null);
+            setImageFile(null);
+            setImageFileUrl(null);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setImageFileUrl(downloadURL);
+            });
+          }
+        );
+      };
       uploadImage();
     }
   }, [imageFile]);
-  const uploadImage = async () => {
-    const storage = getStorage(app);
-    const fileName = new Date().getTime() + imageFile.name;
-    const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, imageFile);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 
-        setImageFileUploadProgress(progress.toFixed(0));
-      },
-      (error) => {
-        setImageFileUploadError(
-          "Could not upload image (File must be less than 2MB)"
-        );
-        setImageFileUploadProgress(null);
-        setImageFile(null);
-        setImageFileUrl(null);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setImageFileUrl(downloadURL);
-        });
-      }
-    );
-  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
